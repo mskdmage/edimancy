913a98ae0cc06245c77c3f4afc8d91ebf2f8c7f6
mskdmage <mskdmage@gmail.com>
2026-02-26 20:35:49 -0500

    feat: Add StreamParser for buffered reading and segment parsing
    - Introduced `StreamParser` struct to handle reading from a buffered input stream.
    - Implemented `Iterator` for `StreamParser` to yield segments based on a specified terminator.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use edimancy::StreamParser;
 use std::{fs::File, io::{BufRead, BufReader, Read}};
 
 fn main() {
 
     let file_path = "tests/samples/sample837.txt";
     let file = File::open(file_path).unwrap();
-    let mut buffered_reader = BufReader::new(file);
-    let mut buffer = Vec::with_capacity(1024);
-    buffered_reader.read_until(b'~',&mut buffer).unwrap();
+    let buffered_reader = BufReader::new(file);
+    let mut parser = StreamParser::new(buffered_reader, 1024, '~');
 
-    println!("{:?}", String::from_utf8(buffer).unwrap());
+    while let Some(segment) = parser.next() {
+        println!("{:?}", String::from_utf8(segment).unwrap());
+    }
 }
--- tests/test_837.rs
+++ tests/test_837.rs
@@ -1,3 +1,20 @@
+use edimancy::StreamParser;
+use std::{fs::File, io::{BufRead, BufReader}};
+
 #[test]
 fn test_parse_837() {
+
+    let file_path = "tests/samples/sample837.txt";
+    let file = File::open(file_path).unwrap();
+    let buffered_reader = BufReader::new(file);
+    let mut parser = StreamParser::new(buffered_reader, 1024, '~');
+    let mut n_segments = 0;
+
+    while let Some(segment) = parser.next() {
+        n_segments += 1;
+    }
+    
+
+    assert!(n_segments == 26); // 26 Expected Segments
+
 }
